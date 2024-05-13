cf15568cd6c7a0035a13477c34f9e9de50346ee5
Oscar Contreras <81644152+raidolContreras@users.noreply.github.com>
2024-05-13 12:39:01 -0600

    buttons

--- view/assets/js/ajax/General/getSchools.js
+++ view/assets/js/ajax/General/getSchools.js
@@ -17,16 +17,26 @@ $(document).ready(function () {
             `;
             data.forEach(school => {
                 html += `
-                    <a href="school&idSchool=${school.idSchool}" class="mt-3 btn btn-success">
-                        <div class="row">
-                            <div class="col-10">${school.nameSchool}</div> 
-                            <div class="col-2">
-                                <i class="fa-solid fa-chevron-right"></i>
-                            </div>
-                        </div>
-                    </a>
+                <a href="school&idSchool=${school.idSchool}" class="mt-3 btn btn-success">
+                    <div style="display: flex; justify-content: space-between; align-items: center;">
+                        <div>${school.nameSchool}</div> 
+                        <i class="fa-solid fa-chevron-right"></i>
+                    </div>
+                </a>
                 `;
             });
+            
+            html += `
+                <a href="plan" class="mt-3 btn btn-success">
+                    <div class="row">
+                        <div class="col-2">
+                            <i class="fa-duotone fa-radar"></i>
+                        </div>
+                        <div class="col-8">Generar plan de supervición</div> 
+                    </div>
+                </a>
+            `;
+
             $('.schools').html(html);
             $('#schools').html(html);
         }
