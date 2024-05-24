47235379b692781f0e60f1b42fbc10edece78bad
Oscar Contreras <81644152+raidolContreras@users.noreply.github.com>
2024-05-24 16:19:08 -0600

    Update forms.models.php

--- model/forms.models.php
+++ model/forms.models.php
@@ -831,7 +831,7 @@ class FormsModel
 									i.idIncidente;
 								");
             $stmt->execute();
-            return $stmt->fetch(PDO::FETCH_ASSOC);
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
         } catch (PDOException $e) {
             error_log("Error al buscar el evento: " . $e->getMessage());
             throw $e;
