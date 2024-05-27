4c1be063de1850c5f894ed5a43c148c74adbd676
Oscar Contreras <81644152+raidolContreras@users.noreply.github.com>
2024-05-27 00:31:22 -0600

    Update editAreas.js

--- view/assets/js/ajax/editAreas.js
+++ view/assets/js/ajax/editAreas.js
@@ -2,15 +2,15 @@ $(document).ready(function(){
 
     $('.update').click(function(e){
         e.preventDefault();
-        var name = $('#nameZoneEdit').val();
-        var idZone = $('#edit').val();
+        var name = $('#nameAreaEdit').val();
+        var idArea = $('#edit').val();
 
         $.ajax({
             type: "POST",
             url: "controller/ajax/ajax.form.php",
             data: {
-                nameZoneEdit: name,
-                idZoneEdit: idZone
+                nameAreaEdit: name,
+                idAreaEdit: idArea
                 },
             success: function(data) {
                 if (data == 'ok'){
