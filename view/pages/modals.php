accf5124f41fe1e8930eb8613e41310d8668226c
Oscar Contreras <81644152+raidolContreras@users.noreply.github.com>
2024-05-26 23:30:31 -0600

    Update modals.php

--- view/pages/modals.php
+++ view/pages/modals.php
@@ -39,3 +39,22 @@
     </div>
 </div>
 
+<div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="modalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title titleEvent" id="eventModalLabel"></h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"><i class="fas fa-times-circle"></i></button>
+            </div>
+			<div class="modal-body contentModal">
+
+			</div>
+            <div class="modal-footer">
+				<div class="center-buttons mt-4">
+					<button type="button" class="btn btn-danger mx-1" data-bs-dismiss="modal">Cancelar</button>
+					<button class="btn btn-primary mx-1" id="modalDeleteButton" data-bs-dismiss="modal">Aceptar</button>
+				</div>
+			</div>
+        </div>
+    </div>
+</div>
