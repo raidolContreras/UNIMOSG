de735b95a9ffaca848b122a3bed40adb4e1ab9fd
Oscar Contreras <81644152+raidolContreras@users.noreply.github.com>
2024-06-21 13:39:19 -0600

    Update generate_vapid_keys.php

--- generate_vapid_keys.php
+++ generate_vapid_keys.php
@@ -5,13 +5,5 @@ use Minishlink\WebPush\VAPID;
 
 $keys = VAPID::createVapidKeys();
 
-$publicKey = $keys['publicKey'];
-$privateKey = $keys['privateKey'];
-
-$fileContent = "<?php\n";
-$fileContent .= "\$publicKey = '" . $publicKey . "';\n";
-$fileContent .= "\$privateKey = '" . $privateKey . "';\n";
-
-file_put_contents('vapid_keys.php', $fileContent);
-
-echo "Archivo vapid_keys.php creado con las claves.\n";
+echo "Clave Pública: " . $keys['publicKey'] . "\n";
+echo "Clave Privada: " . $keys['privateKey'] . "\n";
